b34d37a3ae74c9586420adee5d8348ecd5349d2e
Xinyu <xinyu.hu@essec.edu>
2022-03-21 22:20:22 +0100

    deleted cnn and improved the accuracy

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
-# SentimentAnalysis
+# Sentiment Analysis for reviews of a restaurant
 
-Aspect based sentiment analysis using BERT Classifier and Transformers.
+
+## Authors
+- Xinyu HU(xinyu.hu@student-cs.fr)
+- Hugo VANDERPERRE(hugo.vanderperre@student-cs.fr)
+- Anurag CHATTERJEE(anurag.chatterjee@student-cs.fr)
+
+## 1. Project Description
+This project aims to implement a classifier to predict aspect-based polarities of opinions in reviews of a restaurant.
+
+## 2. Implementation of model
+
+### 2.1 Model selection
+
+The requirement to solve this critical problem is an aspect-based sentiment analysis model.A good amount of research has been done regarding aspect-based models. One can choose either machine learning models  like SVM, logistic regression, random forest, etc.  or deep learning models that are also feasible. We plan to implement a BERT pretrained model because they are predominantly famous in  predicting the opinion on specific aspects relying on the high performance of the bert family for aspect-based sentiment analysis.
+
+### 2.2 Requirement
+
+- transformers==4.17.0
+- Pytorch==1.10.0
+- spacy==2.2.4
+
+### 2.3 Summary of Final model
+
+All aspects and reviewed objects and completed reviewed sentences are tokenized and fed into the pretrained Bert model. Then, the polarities of the comments are converted to integers, in which way it is easier to compute the loss. The final model consists in a pretrained BERT model (bert base uncased) applied with an Adam optimizer with linear scheduled warm up.
+
+### 2.4 Results
+
+Finally we achieved accuracy **[76.86%, 77.13%, 76.33%,  77.93%,  76.06%]** of deviation set in 5 runs. Mean deviation accuracy is **76.86% ** using 332 seconds per run in GPU.
+
+### 2.5 Improvement
+
+Based on the time limit, we could only explore the single Bert pretrained model and Bert pretrained model based on a shallow CNN. Since  CNN performed relatively worse, we removed this part from our final model to avoid loss of accuracy. In future, we can try to implement bert based on LSTM to further customize the bert model and improve the predicted accuracy.
+
+We also tried Bert large uncased pretrained model, expecting to get higher accuracy but the result was the other way round. A suspicious overfitting happened in that case,thus allowing us to ponder that  we can try to combine some based model with bert large to explore more probabilities.
--- src/classifier.py
+++ src/classifier.py
@@ -2,55 +2,24 @@ import numpy as np
 import torch
 from transformers import BertTokenizer, BertForSequenceClassification, BertConfig
 from transformers import AdamW
-from transformers import WarmupLinearSchedule as get_linear_schedule_with_warmup
+from transformers import get_linear_schedule_with_warmup
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 from torch.nn.functional import softmax
 import torch.nn as nn
 from collections import Counter
 from tqdm import tqdm
 import spacy
-#import en_core_web_sm
 import time
 import datetime
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 nlp = spacy.load("en_core_web_sm")
 
-# This is the classifier to be added to the BERT model
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.kernel_size = 24
-        self.pooling = nn.AvgPool1d(self.kernel_size)  # Average pooling layer
-        self.fc = nn.Linear(768 // self.kernel_size, 3)  # Fully connected layer
-
-    def forward(self, x):
-        x = x.view(-1, 1, 768)  # Need to reshape x before giving it to the pooling layer
-        x = self.pooling(x)
-        x = torch.relu(x.view(-1, 768//self.kernel_size))
-        x = self.fc(x)
-        return x
-
-
-def format_time(elapsed):
-    '''
-    Takes a time in seconds and returns a string hh:mm:ss
-    '''
-    # Round to the nearest second.
-    elapsed_rounded = int(round((elapsed)))
-
-    # Format as hh:mm:ss
-    return str(datetime.timedelta(seconds=elapsed_rounded))
-
 
 class Classifier:
-    """The Classifier"""
 
-    #############################################
-    def __init__(self, train_batch_size=16, eval_batch_size=8, max_length=128, lr=2e-5, eps=1e-6, n_epochs=11):
+    def __init__(self, train_batch_size=12, eval_batch_size=8, max_length=128, lr=2e-5, eps=1e-5, n_epochs=10):
         """
-
         :param train_batch_size: (int) Training batch size
         :param eval_batch_size: (int) Batch size while using the `predict` method.
         :param max_length: (int) Maximum length for padding
@@ -75,96 +44,46 @@ class Classifier:
         # Tokenizer
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-        # The model #
-        #   We first need to specify some configurations to the model
-        configs = BertConfig.from_pretrained('bert-base-uncased', num_labels=3, type_vocab_size=8)  # BERT configuration
+        # define the pretrained model
+        configs = BertConfig.from_pretrained('bert-base-uncased', num_labels=3)  # BERT configuration
         self.model = BertForSequenceClassification(configs)
+        self.model.to(device)
 
-        #   We are changing the header classifier of the model (Which is initially a simple fully connect layer layer)
-        clf = Net()
-        self.model.classifier = clf
+    def train(self, trainfile, devfile):
 
-        self.model.to(device)  # putting the model on GPU if available otherwise device is CPU
-
-    def preprocess(self, sentences):
-        """
-        The preprocessing function
-        :param sentences: List of all sentences to be given at once.
-        :return: List of preprocessed sentences.
-        """
-
-        preprocessed = []
-        for sentence in tqdm(sentences):
-
-            assert isinstance(sentence, str)
-            doc = nlp(str(sentence))
-            tokens = []
-            for token in doc:
-                if (not token.is_punct) or (token.text not in [',', '-', '.', "'", '!']):  # Some punctuations can be interesting for BERT
-                    tokens.append(token.text)
-            tokens = (' '.join(tokens)).lower().replace(" '", "'")
-            preprocessed.append(tokens)
-
-        return preprocessed
-
-    def question(self, category):
-        """
-        Computes the questions corresponding to each category
-        :param category: (str) The category/aspect
-        :return: (str) computed question using the QA-M task
-        """
-        assert category in self.categories
-
-        if category == 'AMBIENCE#GENERAL':
-            return "what do you think of the ambience of it ?"
-        elif category == 'DRINKS#PRICES' or category == 'FOOD#PRICES' or category == 'RESTAURANT#PRICES':
-            return "what do you think of the price of it ?"
-        elif category == 'DRINKS#QUALITY' or category == 'FOOD#QUALITY':
-            return "what do you think of the quality of it ?"
-        elif category == 'DRINKS#STYLE_OPTIONS':
-            return "what do you think of drinks ?"
-        elif category == 'FOOD#STYLE_OPTIONS':
-            return "what do you think of the food ?"
-        elif category == 'LOCATION#GENERAL':
-            return "what do you think of the location of it ?"
-        elif category == 'RESTAURANT#GENERAL' or category == 'RESTAURANT#MISCELLANEOUS':
-            return "what do you think of the restaurant ?"
-        elif category == 'SERVICE#GENERAL':
-            return "what do you think of the service of it ?"
-
-    def train(self, trainfile):
         """Trains the classifier model on the training set stored in file trainfile"""
 
         # Loading the data and splitting up its information in lists
-        print("\n   Loading training data...")
         trainset = np.genfromtxt(trainfile, delimiter='\t', dtype=str, comments=None)
         self.trainset = trainset
+
         n = len(trainset)
+
+        # get the opinions
         targets = trainset[:, 0]
+        self.labels = list(Counter(targets).keys())
+
+        # get the aspects
         categories = trainset[:, 1]
-        self.labels = list(Counter(targets).keys())  # label names
-        self.categories = list(Counter(categories).keys())  # category names
+        self.categories = list(Counter(categories).keys())
+
+        # get the reviwed objects
+        objects = trainset[:,2]
+
+        # get the words from start to end
         start_end = [[int(x) for x in w.split(':')] for w in trainset[:, 3]]
-        # target words
-        words_of_interest = [trainset[:, 4][i][start_end[i][0]:start_end[i][1]] for i in range(n)]
-        # sentences to be classified
+        words = [trainset[:, 4][i][start_end[i][0]:start_end[i][1]] for i in range(n)]
+
+        # get the complete reviews
         sentences = [str(s) for s in trainset[:, 4]]
-
-        # Preprocessing the text data
-        print("   Preprocessing the text data...")
-        sentences = self.preprocess(sentences)
-
-        # Computing question sequences
-        print("   Computing questions...")
-        questions = [self.question(categories[i]) for i in tqdm(range(n))]
 
         # Tokenization
         attention_masks = []
         input_ids = []
         token_type_ids = []
         labels = []
-        for word, question, answer in zip(words_of_interest, questions, sentences):
-            encoded_dict = self.tokenizer.encode_plus(answer, question + ' ' + word.lower(),
+        for category, obj, word, sentence in zip(categories, objects, words, sentences):
+            encoded_dict = self.tokenizer.encode_plus(category, obj+' '+word.lower()+ ' ' + sentence.lower(),
                                                       add_special_tokens=True,  # Add '[CLS]' and '[SEP]' tokens
                                                       max_length=self.max_length,  # Pad & truncate all sequences
                                                       pad_to_max_length=True,
@@ -195,84 +114,99 @@ class Classifier:
                                       batch_size=self.train_batch_size,
                                       sampler=train_sampler)
 
-        # Optimizer and scheduler (we are using a linear scheduler without warm up)
-        no_decay = ['bias', 'gamma', 'beta']  # These parameters are not going to be decreased
+        # Optimizer and scheduler
+        no_decay = ['bias', 'gamma', 'beta']
+
         optimizer_parameters = [
             {'params': [p for n, p in self.model.named_parameters() if not any(nd in n for nd in no_decay)],
              'weight_decay': 0.01},
             {'params': [p for n, p in self.model.named_parameters() if any(nd in n for nd in no_decay)],
              'weight_decay': 0.0}
         ]
+         # In Transformers, optimizer and schedules are split and instantiated like this:
         optimizer = AdamW(optimizer_parameters,
                           lr=self.lr,
                           eps=self.eps)
+
+        # optimizer = torch.optim.SGD(self.model.parameters(), lr=0.01, momentum=0.9)
+
         total_steps = len(train_dataloader) * self.n_epochs
+
         scheduler = get_linear_schedule_with_warmup(optimizer,
                                                     num_warmup_steps=0,
                                                     num_training_steps=total_steps)
 
-        # Training
         initial_t0 = time.time()
         for epoch in range(self.n_epochs):
             print('\n   ======== Epoch %d / %d ========' % (epoch + 1, self.n_epochs))
             print('   Training...\n')
-            t0 = time.time()
+
             total_train_loss = 0
 
             self.model.train()
+
             for step, batch in enumerate(train_dataloader):
                 batch = tuple(t.to(device) for t in batch)
+
                 input_ids_, input_mask_, segment_ids_, label_ids_ = batch
 
                 self.model.zero_grad()
-                loss, _ = self.model(input_ids_,
+
+                # compute the loss
+                loss = self.model(input_ids_,
                                      token_type_ids=segment_ids_,
                                      attention_mask=input_mask_,
-                                     labels=label_ids_)
+                                     labels=label_ids_).loss
+
+                # Accumulate the training loss over all of the batches
+                # so that we can calculate the average loss at the end.
                 total_train_loss += loss.item()
 
+                # Perform a backward pass to calculate the gradients.
                 loss.backward()
-                # clip gradient norm
+
+                # clip gradient norm to 1.0
+                # This is to help prevent the "exploding gradients" problem.
                 torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
 
                 optimizer.step()
+
                 scheduler.step()
 
+            # Calculate the average loss over all of the batches.
             avg_train_loss = total_train_loss / len(train_dataloader)
-            training_time = format_time(time.time() - t0)
             # print("     Average training loss: {0:.2f}".format(avg_train_loss))
-            print("     Training epoch duration: {:}".format(training_time))
-        print("     Total training time: {:}".format(format_time(time.time() - initial_t0)))
 
     def predict(self, datafile):
-        """Predicts class labels for the input instances in file 'datafile'
+
+        """
+        Predicts class labels for the input instances in file 'datafile'
         Returns the list of predicted labels
         """
 
         # Loading the data and splitting up its information in lists
         evalset = np.genfromtxt(datafile, delimiter='\t', dtype=str, comments=None)
         m = len(evalset)
+
+        # get categories
         categories = evalset[:, 1]
+
+        # get the reviwed objects
+        objects = evalset[:,2]
+
+        # get words from start to end
         start_end = [[int(x) for x in w.split(':')] for w in evalset[:, 3]]
-        # target words
-        words_of_interest = [evalset[:, 4][i][start_end[i][0]:start_end[i][1]] for i in range(m)]
-        # sentences to be classified
+        words = [evalset[:, 4][i][start_end[i][0]:start_end[i][1]] for i in range(m)]
+
+        # get the complete review sentences
         sentences = [str(s) for s in evalset[:, 4]]
-
-        # Preprocessing the text data
-        print("\n   Preprocessing the text data...")
-        sentences = self.preprocess(sentences)
-
-        # Computing question sequences
-        print("   Computing questions...")
-        questions = [self.question(categories[i]) for i in tqdm(range(m))]
 
         # Tokenization
         attention_masks = []
         input_ids = []
         token_type_ids = []
-        for word, question, answer in zip(words_of_interest, questions, sentences):
-            encoded_dict = self.tokenizer.encode_plus(answer, question + ' ' + word.lower(),
+        for category, obj, word, sentence in zip(categories, objects, words, sentences):
+            encoded_dict = self.tokenizer.encode_plus(category, obj+' '+word.lower()+' '+sentence.lower(),
                                                       add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
                                                       max_length=self.max_length,  # Pad & truncate all sequences
                                                       pad_to_max_length=True,
@@ -295,7 +229,9 @@ class Classifier:
 
         # Prediction
         named_labels = []
+
         self.model.eval()
+
         for batch in eval_dataloader:
             batch = tuple(t.to(device) for t in batch)
             input_ids, input_mask, segment_ids = batch
